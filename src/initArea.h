d263cea13ae1566e97187d033680ed9ffdf55a0b
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-18 11:06:26 +0200

    Update initArea.h

--- src/initArea.h
+++ src/initArea.h
@@ -7,6 +7,15 @@
 #include "types_rikaya.h"
 #include "p1.5test_rikaya_v0.c"
 
+/* Lista dei processi ready */
+extern INIT_LIST(ready_queue);
+/* Puntatore al processo atti,vo */
+extern pcb_t* current_process
+/* Contatore processi */
+extern u32 process_count;
+/* Contatore processi bloccati per I/O */
+extern u32 soft_block_count;
+
 HIDDEN inline void initSYS(u32 status);
 HIDDEN inline void initPGMTRP(u32 status);
 HIDDEN inline void initINT(u32 status);
