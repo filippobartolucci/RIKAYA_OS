fa8426806003d507b7b769473e55569d2b367c8f
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-19 19:04:26 +0200

    Update initArea.h

--- src/initArea.h
+++ src/initArea.h
@@ -1,31 +1,39 @@
-#include "pcb.h"
-#include "const.h"
+#ifndef _INITAREA_H
+#define _INITAREA_H
+
 #include "handler.h"
-#include "interrupt.h"
 #include "listx.h"
-#include "scheduler.h"
 #include "types_rikaya.h"
 
-
 /* Lista dei processi ready */
-extern INIT_LIST(ready_queue);
-/* Puntatore al processo atti,vo */
-extern pcb_t* current_process
+struct list_head ready_queue;
+/* Puntatore al processo attivo */
+pcb_t* current_process;
 /* Contatore processi */
-extern u32 process_count;
+u32 process_count;
 /* Contatore processi bloccati per I/O */
-extern u32 soft_block_count;
+u32 soft_block_count;
 
 /* Puntatori alle NEW AREA della ROM */
-extern state_t *sysbk_newarea = SYSBK_NEWAREA;
-extern state_t *program_trap_newarea = PRGTRP_NEWAREA;
-extern state_t *interrupt_newarea = INT_NEWAREA;
-extern state_t *tblmgt_newarea = TLB_NEWAREA;
+state_t *sysbk_newarea;
+state_t *program_trap_newarea;
+state_t *interrupt_newarea;
+state_t *tblmgt_newarea;
 
+/* Puntatori alle OLD AREA della ROM */
+state_t *sysbk_oldarea;
+state_t *program_trap_oldarea;
+state_t *interrupt_oldarea;
+state_t *tblmgt_oldarea;
 
 HIDDEN inline void initSYS(u32 status);
 HIDDEN inline void initPGMTRP(u32 status);
 HIDDEN inline void initINT(u32 status);
 HIDDEN inline void initTLB(u32 status);
+
+void initAREA(void);
 void initNEWAREA(void);
+
+#endif
+
 
