501328f25486b4b7b414708c6ec4ec3b225c2f69
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-19 19:02:22 +0200

    Update handler.h

--- src/handler.h
+++ src/handler.h
@@ -1,12 +1,14 @@
-#include "pcb.h"
-#include "const.h"
-#include "handler.h"
+#ifndef _HANDLER_H
+#define _HANDLER_H
+
 #include "listx.h"
-#include "types_rikaya.h"
-#include "p1.5test_rikaya_v0.c"
+#include "utils.h"
+#include "initArea.h"
 
 /* Funzioni per la gestione delle eccezioni */
 void sysbk_handler(void);
 void int_handler(void);
 void tlb_handler(void);
 void pgmtrp_handler(void);
+
+#endif
