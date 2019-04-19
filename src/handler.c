a64995fa2d995b406f439b539f07dd279bc5a3a4
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-19 19:01:49 +0200

    Update handler.c

--- src/handler.c
+++ src/handler.c
@@ -1,9 +1,4 @@
-#include "pcb.h"
-#include "const.h"
 #include "handler.h"
-#include "listx.h"
-#include "types_rikaya.h"
-#include "p1.5test_rikaya_v0.c"
 
 /* Gestione SYSCALL/BREAKPOINT */
 void sysbk_handler(void){
