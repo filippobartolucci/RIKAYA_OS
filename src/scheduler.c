5e463bd5f7dd218bf3c7bc8480ecc6454e181993
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-21 19:09:47 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -4,7 +4,7 @@
 void scheduler(void) {
 	termprint("inizio scheduler\n", 0);
 	/*gestione dei deadlock poi dei processi*/
-	if (current_process == NULL){
+	/*if (current_process == NULL){
 		if(process_count == 0)
 			HALT();
 		current_process = removeProcQ(&ready_queue);
@@ -13,7 +13,19 @@ void scheduler(void) {
 		log_process_order(current_process->original_priority);
 		setTIMER(TIMESLICE);
 		LDST(&current_process->p_s);
-	}	
+	}*/
+	pcb_t *old = outProcQ(&ready_queue, current_process);
+	
+	if (old){
+		memcpy(interrupt_oldarea, &old->p_s, sizeof(state_t);
+		restorePriority(old);
+		insertProc"(&ready_queue,current);
+ 	}     
+		       
+	current_process = headProcQ(&ready_queue);       
+	setTIMER(3000);
+	log_process_order(current_process->original_priority)
+	LDST(&current_process);
 }
 
 /* Funzione che si occupa del meccanismo di aging delle priorità dei PCB nella ready queue */
