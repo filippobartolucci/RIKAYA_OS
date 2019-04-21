480cbb88aedf5db8fefd99f0b282a7fcef87af0f
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-21 10:10:09 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -14,11 +14,16 @@ void scheduler(void)
 		log_process_order(current_process->original_priority);
 		setTIMER(TIMESLICE);
 		LDST(current_process->p_s);
-	}
-		
+	}else{
+		pcb_t *new_proc = removeProcQ(&ready_queue);
+		restorePriority(new_proc);
+		priorityAging();
+		insertProcQ(&ready_queue,current_process);
+		log_process_order(new_proc->original_priority);
+		current_process = new_proc;
+		setTIMER(TIMESLICE);
+		LDST(current_process->p_s);	
 }
-
-// restorePriority e priorityAgin vanno chiamate dopo aver estratto il processo (con priorità massima) dalla readyqueue
 
 /* Funzione che si occupa del meccanismo di aging delle priorità dei PCB nella ready queue */
 HIDDEN inline void priorityAging(void) {
@@ -27,36 +32,9 @@ HIDDEN inline void priorityAging(void) {
 		pcb_t *tmp = container_of(iter, pcb_t, p_next);
 		if (tmp->priority<20) tmp->priority++;
 		}
-	
-		
-	
-	
-	//if (!list_empty(ready_queue)) {
-    		/* PCB temporaneo che uso per scorrere la ready_queue */
-	//	pcb_t *tmp = readyQueue;
-		/* Scorro tra processi che già hanno priorità massima */
-	//	while (tmp->p_priority == MAXPRIO && tmp->p_next != NULL) tmp = tmp->p_next;
-		/* Scorro tra i processi che non hanno priorità massima e la aumento */
-	//	while (tmp->p_next != NULL) {
-	//		tmp->p_priority++;
-	//		tmp = tmp->p_next;
-	//	}
-	//}
-	/*
-	pcb_t* tmp;
-	tmp = headProcQ(&ready_queue);
-	int i = 0;
-	list_for_each_entry(tmp, &tmp->p_next , p_next){
-		if(tmp->priority < MAXPRIO)
-			tmp->priority++;
-	}
-	*/
-
 }
 
 /* Funzione che si occupa di ripristinare la priorità originale di un PCB */
 HIDDEN inline void restorePriority(pcb_t *pcb){
 	pcb->priority = pcb->original_priority; 	
 }
-//gestire un solo inturupt quello del tempo
-//l'unica cosa che esegue lo scheduler e il passaggio da un processo ad un altro
