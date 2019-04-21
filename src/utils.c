2dfbd4d834934a6601b4e4c1db355054e3ef05a1
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-21 19:11:44 +0200

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,3 +6,13 @@ void *memset(void *s, u32 c, int n){
       *p++ = (unsigned char)c;
   return s;
 }
+
+void memcopy(void *dest, void *src, size_t n) { 
+   // Typecast src and dest addresses to (char *) 
+   char *csrc = (char *)src; 
+   char *cdest = (char *)dest; 
+  
+   // Copy contents of src[] to dest[] 
+   for (int i=0; i<n; i++) 
+       cdest[i] = csrc[i]; 
+} 
