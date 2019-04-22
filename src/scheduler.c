780284bf1ff3479cb2b82867bcf6c34a6f0e7eb2
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-22 11:03:32 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -2,31 +2,16 @@
 #include "scheduler.h"
 
 void scheduler(void) {
-	termprint("inizio scheduler\n", 0);
-	/*gestione dei deadlock poi dei processi*/
-	/*if (current_process == NULL){
-		if(process_count == 0)
-			HALT();
-		current_process = removeProcQ(&ready_queue);
-		restorePriority(current_process);
-		priorityAging();
-		log_process_order(current_process->original_priority);
-		setTIMER(TIMESLICE);
-		LDST(&current_process->p_s);
-	}*/
 	pcb_t *old = outProcQ(&ready_queue, current_process);
-	
 	if (old){
 		memcpy(interrupt_oldarea, &old->p_s, sizeof(state_t));
 		restorePriority(old);
 		insertProcQ(&ready_queue,current_process);
- 	}     
-		       
-	current_process = headProcQ(&ready_queue);   
-    	termprint("timer\n",0);
+ 	}     	       
+	current_process = headProcQ(&ready_queue);
+	priorityAging();
 	setTIMER(TIMESLICE * TIMESCALE);
 	log_process_order(current_process->original_priority);
-    	termprint("ldst",0);
 	LDST(&current_process->p_s);
 }
 
