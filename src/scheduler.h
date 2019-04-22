f192e9b9fd49db62c5c24bb834f3bd42edea9ac5
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-22 21:50:40 +0200

    Update scheduler.h

--- src/scheduler.h
+++ src/scheduler.h
@@ -3,10 +3,13 @@
 
 #include "pcb.h"
 #include "initArea.h"
+#include "utils.h"
 
 void scheduler();
 
 HIDDEN inline void priorityAging(void);
 HIDDEN inline void restorePriority(pcb_t *pcb);
+
+extern void log_process_order(u32 n);
 
 #endif 
