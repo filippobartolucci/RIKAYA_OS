c0da1bdc548dc840784c33c846e8434d3e5a6023
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-22 21:46:29 +0200

    Update handler.h

--- src/handler.h
+++ src/handler.h
@@ -4,6 +4,7 @@
 #include "listx.h"
 #include "utils.h"
 #include "initArea.h"
+#include "scheduler.h"
 
 /* Funzioni per la gestione delle eccezioni */
 void sysbk_handler(void);
@@ -11,4 +12,5 @@ void int_handler(void);
 void tlb_handler(void);
 void pgmtrp_handler(void);
 
+HIDDEN void terminateProcess();
 #endif
