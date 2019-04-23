e4ebd6a2421336ec736a57f94e172edd7c732ff1
francesco_cerio <francescocerio98@gmail.com>
2019-04-23 19:17:04 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -5,9 +5,7 @@
  *						*
  * 	 Componenti del gruppo:	   		*
  *	   - Filippo Bartolucci	   		*
- *	   - Francesco Cerio		  	*
- *	   - Umberto Case		   	*
- *	   - Matteo Celani		   	*/
+ *	   - Francesco Cerio		     	*/
  
 #include "listx.h"
 #include "scheduler.h"
