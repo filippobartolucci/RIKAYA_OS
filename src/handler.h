d21e1d70a98cc5b8d8ec89e72abe0f43a5b7437c
francesco_cerio <francescocerio98@gmail.com>
2019-07-26 17:26:43 +0200

    Update handler.h

--- src/handler.h
+++ src/handler.h
@@ -14,6 +14,7 @@
 #include "utils.h"
 #include "initArea.h"
 #include "scheduler.h"
+#include "asl.h"
 
 /* Funzioni per la gestione delle eccezioni */
 void sysbk_handler(void);
@@ -21,5 +22,8 @@ void int_handler(void);
 void tlb_handler(void);
 void pgmtrp_handler(void);
 
+HIDDEN void getCpuTime(unsigned int* user, unsigned int* kernel, unsigned int* wallclock);
+HIDDEN int createProcess(state_t* statep, int priority, void** cpid);
+HIDDEN void verhogen(int* semaddr);
 HIDDEN void terminateProcess();
 #endif
