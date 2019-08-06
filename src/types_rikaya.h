8ed42f32f099494995732e829a0f8474f79eee09
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-08-06 15:31:49 +0200

    Update types_rikaya.h

--- src/types_rikaya.h
+++ src/types_rikaya.h
@@ -23,8 +23,7 @@ typedef struct pcb_t {
 	/* process priority */
 	int			priority;
 	int 		original_priority;
-    
-    BOOL tutor;
+	bool tutor;
 
 	/* key of the semaphore on which the process is eventually blocked */
 	int			*p_semkey;
