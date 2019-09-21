f2e830166e80003eabb6675ad0b5f3a478479101
francesco_cerio <francescocerio98@gmail.com>
2019-09-21 12:49:23 +0200

    Add files via upload

---
+++ phase2-fra/main.c
@@ -0,0 +1,100 @@
+
+/*						*
+ *		 PHASE1 RIKAYA	   		*
+ *						*
+ * 	 Sviluppato dal gruppo lso19az22 	*
+ *						*
+ * 	 Componenti del gruppo:	   		*
+ *	   - Filippo Bartolucci	   		*
+ *	   - Francesco Cerio		     	*/
+
+#include "pcb.h"
+#include "const.h"
+#include "handler.h"
+#include "listx.h"
+#include "scheduler.h"
+#include "types_rikaya.h"
+#include "initArea.h"
+#include "utils.h"
+#include "asl.h"
+
+/* Traced Regions */
+u32 debug = 0;
+u32 debug2 = 0;
+/* Funzioni di test per PHASE2 */
+extern void test();
+
+void dummy(){
+	state_t proc;
+	debug = 0xdadda;
+	memset(&proc, 0, sizeof(proc));
+	proc.pc_epc = (u32)test;
+	proc.reg_sp = RAMTOP - FRAME_SIZE *2;
+	debug = 0xdddd;
+	proc.status = (1<<2|1<<27|0xFF00|0x1);
+	debug = 0xcacca;
+	SYSCALL(SETTUTOR, 0, 0,0);
+	debug = 0xacca;
+	SYSCALL(CREATEPROCESS, (u32)&proc,1,0);
+	debug = 0xaffa;
+	while(1);
+}
+
+/* Lista dei processi ready */
+LIST_HEAD(ready_queue);
+/* Puntatore al processo attivo */
+pcb_t* current_process = NULL;
+/* Contatore processi */
+u32 process_count = 0;
+
+/* Puntatori alle NEW AREA della ROM */
+state_t *sysbk_newarea = (state_t *)SYSBK_NEWAREA;
+state_t *program_trap_newarea = (state_t *)PGMTRAP_NEWAREA;
+state_t *interrupt_newarea = (state_t *)INT_NEWAREA;
+state_t *tlbmgt_newarea = (state_t *)TLB_NEWAREA;
+
+/* Puntatori alle OLD AREA della ROM */
+state_t *sysbk_oldarea = (state_t *)SYSBK_OLDAREA;
+state_t *program_trap_oldarea = (state_t *)PGMTRAP_OLDAREA;
+state_t *interrupt_oldarea = (state_t *)INT_OLDAREA;
+state_t *tlbmgt_oldarea = (state_t *)TLB_OLDAREA;
+
+
+void setProcess(u32 proc, int prio, int frame){
+    /* Prendo un PCB dalla lista dei PCB liberi */
+	pcb_t *tmp = allocPcb();
+    /* Imposto il PROGRAM COUNTER del processo */
+	tmp->p_s.pc_epc = tmp->p_s.reg_t9 = proc;
+    /* Imposto la priorità */
+	tmp->priority = tmp->original_priority = prio;
+    /* Imposto lo STACK POINTER */
+	tmp->p_s.reg_sp = RAMTOP - FRAME_SIZE *frame;
+    /* Imposto lo STATUS del process */
+	setSTATUS(getSTATUS()|0|1<<27|0xFF<<8);
+
+	tmp->total_time = TOD_LO;
+
+    /* Aumento il contatore dei processi */
+	process_count++;
+    /* Inserisco il PCB nella lista dei processi in stato ready */
+	insertProcQ(&ready_queue, tmp);
+}
+
+int main(void){
+    /* Inizializzazione del sistema */
+    initAREA();
+    initPcbs();
+    initASL();
+    /* Setto Interval Timer */
+    *((u32 *)INT_TIMER) = (u32)PSEUDO_CLOCK_TICK;
+
+    debug = 1;
+    setProcess(dummy, 0, 1);
+    //debug = 2;
+    /* Passo il controllo allo scheduler */
+    scheduler();
+
+    /* L'esecuzione non deve mai arrivare qui */
+    PANIC();
+    return 0;
+}
---
+++ phase2-fra/scheduler.c
@@ -0,0 +1,75 @@
+/*						*
+ *		 PHASE1 RIKAYA	   		*
+ *						*
+ * 	 Sviluppato dal gruppo lso19az22 	*
+ *						*
+ * 	 Componenti del gruppo:	   		*
+ *	   - Filippo Bartolucci	   		*
+ *	   - Francesco Cerio		     	*/
+
+#include "listx.h"
+#include "scheduler.h"
+
+
+int debug_s = 0;
+int debug_s1 = 0;
+void scheduler(void) {
+
+    /* Ultimo processo in esecuzione */
+    pcb_t *old = outProcQ(&ready_queue, current_process);
+    pcb_t* next;
+
+    /* Se old != NULL */
+	if (old){	
+		debug_s1++;
+        /* Ripristino la sua priorità */
+		restorePriority(old);
+        /* Salvo lo stato dell'esecuzione prima dell'eccezione */
+		memcpy(&old->p_s,(state_t *)INT_OLDAREA, sizeof(state_t));
+	debug_s1++;
+        old->kernel_time += TOD_LO - current_process->kernel_time_start;
+        old->kernel_time = 0;
+
+        /* Reinserisco il processi nella ready_queue */
+        insertProcQ(&ready_queue,old);
+ 	}
+/* Controllo se ci sono ancora processi da eseguire */
+    checkEmptyProcQ();
+    /* Estraggo il processo con priorità più alta dalla ready_queue */
+	next = headProcQ(&ready_queue);
+
+    /* Aumento la priorità dei processi che sono nella ready_queue */
+    priorityAging();
+    /* Imposto il PLT */
+    setTIMER(TIMESLICE * TIME_SCALE);
+    /* Context switch */
+    current_process = next;
+    /* Aggiorno il tempo */
+    next->user_time_start = TOD_LO;
+    debug_s++;
+    /* Carico lo stato del processo corrente */
+    LDST(&current_process->p_s);
+}
+
+/* Funzione ausiliaria per verificare se il processo corrente è vuoto */
+HIDDEN inline void checkEmptyProcQ(void){
+    if(current_process == NULL && list_empty(&ready_queue)){
+	    debug_s =0xacaca; 
+	setSTATUS(getSTATUS()|(1UL)|0xFF<<8|0);
+    	WAIT();
+    }
+}
+
+/* Funzione che si occupa del meccanismo di aging delle priorità dei PCB nella ready queue */
+HIDDEN inline void priorityAging(void) {
+	struct list_head *iter;
+	list_for_each(iter, &ready_queue){
+		pcb_t *tmp = container_of(iter, pcb_t, p_next);
+		if (tmp->priority < MAXPRIO) tmp->priority++;
+	}
+}
+
+/* Funzione che si occupa di ripristinare la priorità originale di un PCB */
+HIDDEN inline void restorePriority(pcb_t *pcb){
+	pcb->priority = pcb->original_priority;
+}
