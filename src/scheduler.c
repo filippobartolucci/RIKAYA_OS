abebd6dc9a055d6bd3b17b16929a19dd30d7e972
francesco_cerio <francescocerio98@gmail.com>
2019-09-21 12:32:35 +0200

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -11,7 +11,8 @@
 #include "scheduler.h"
 
 
-
+int debug_s = 0;
+int debug_s1 = 0;
 void scheduler(void) {
 
     /* Ultimo processo in esecuzione */
@@ -19,24 +20,23 @@ void scheduler(void) {
     pcb_t* next;
 
     /* Se old != NULL */
-	if (old){
+	if (old){	
+		debug_s1++;
         /* Ripristino la sua priorità */
 		restorePriority(old);
         /* Salvo lo stato dell'esecuzione prima dell'eccezione */
 		memcpy(&old->p_s,(state_t *)INT_OLDAREA, sizeof(state_t));
-
+	debug_s1++;
         old->kernel_time += TOD_LO - current_process->kernel_time_start;
         old->kernel_time = 0;
 
         /* Reinserisco il processi nella ready_queue */
         insertProcQ(&ready_queue,old);
  	}
-
+/* Controllo se ci sono ancora processi da eseguire */
+    checkEmptyProcQ();
     /* Estraggo il processo con priorità più alta dalla ready_queue */
 	next = headProcQ(&ready_queue);
-
-    /* Controllo se ci sono ancora processi da eseguire */
-    checkEmptyProcQ();
 
     /* Aumento la priorità dei processi che sono nella ready_queue */
     priorityAging();
@@ -46,15 +46,18 @@ void scheduler(void) {
     current_process = next;
     /* Aggiorno il tempo */
     next->user_time_start = TOD_LO;
+    debug_s++;
     /* Carico lo stato del processo corrente */
     LDST(&current_process->p_s);
 }
 
 /* Funzione ausiliaria per verificare se il processo corrente è vuoto */
 HIDDEN inline void checkEmptyProcQ(void){
-    if(current_process == NULL && list_empty(&ready_queue))
-        setSTATUS(getSTATUS()|1UL);
-	WAIT();
+    if(current_process == NULL && list_empty(&ready_queue)){
+	    debug_s =0xacaca; 
+	setSTATUS(getSTATUS()|(1UL)|0xFF<<8|0);
+    	WAIT();
+    }
 }
 
 /* Funzione che si occupa del meccanismo di aging delle priorità dei PCB nella ready queue */
@@ -70,3 +73,4 @@ HIDDEN inline void priorityAging(void) {
 HIDDEN inline void restorePriority(pcb_t *pcb){
 	pcb->priority = pcb->original_priority;
 }
+
