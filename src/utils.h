68242d3f3dea4ba5653ae482557c3bfc156326d9
filippobartolucci <filippobartolucci@me.com>
2019-10-09 20:54:53 +0200

    P3 si blocca in GETCPUTIME

--- src/utils.h
+++ src/utils.h
@@ -25,6 +25,8 @@ extern void PANIC();
 extern void setTIMER(u32 n);
 extern void LDST(state_t *s);
 extern void SYSCALL(int *a0, int *a1, int *a3, int *a4);
+extern int getSTATUS();
+extern int setSTATUS();
 
 
 
